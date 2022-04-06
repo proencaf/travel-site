248cfe3e46d8e215d3ed70623859e42563ee9fe6
proencaf <83221947+proencaf@users.noreply.github.com>
2022-04-06 11:53:35 +0100

    NavBar component + styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import './NavBar.css'
+
+const NavBar = () => {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+
+  return (
+    <div className='header'>
+      <Link to='/'>
+        <h1>Remote TRVL</h1>
+      </Link>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/pricing'>Pricing</Link>
+        </li>
+        <li>
+          <Link to='/activities'>Activities</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
+      <div className='hamburger' onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: '#fff' }} />
+        ) : (
+          <FaBars size={20} style={{ color: '#fff' }} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
